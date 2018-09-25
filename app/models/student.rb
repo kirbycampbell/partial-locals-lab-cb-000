60ef9b454532923ea90e34d1c626ea1762562d06
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-25 00:37:39 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,9 +13,6 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
-  validates :name, presence: true
-  validates :hometown, presence: true
-  validates :birthday, presence: true
 
 
   def self.search(name)
